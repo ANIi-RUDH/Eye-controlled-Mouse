0d1ebdf35c4f2850266ad0f4f4e5e1ca981a5fde
ANI-RUDH <117719175+ANIi-RUDH@users.noreply.github.com>
2024-06-04 10:11:52 +0530

    Update Project.py

--- Project.py
+++ Project.py
@@ -2,8 +2,6 @@ import cv2
 import mediapipe as mp
 import pyautogui
 
-# To close the camera press "CTRL + F2"
-
 cam = cv2.VideoCapture(0)
 face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
 screen_w, screen_h = pyautogui.size()
